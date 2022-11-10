557873e83f937c0133415467de6ca1244b3b1ea6
Danny <90986300+djecodes@users.noreply.github.com>
2022-11-09 22:23:29 -0600

    Create annalyns-infiltration.js

---
+++ Annalyn's Filtration/annalyns-infiltration.js
@@ -0,0 +1,10 @@
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
+export function canSpy()
