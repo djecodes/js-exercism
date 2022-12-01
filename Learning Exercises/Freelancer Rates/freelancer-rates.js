92255e74ae0c34cb404581afdfeb66e6767895e3
Danny <90986300+djecodes@users.noreply.github.com>
2022-12-01 17:50:57 -0600

    Add boilerplate code, set up dayRate function

--- Learning Exercises/Freelancer Rates/freelancer-rates.js
+++ Learning Exercises/Freelancer Rates/freelancer-rates.js
@@ -1,1 +1,28 @@
-//Enter Code Here
+/**
+ * The day rate, given a rate per hour
+ * 
+ * @param {number} ratePerHour
+ * @return {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+    return ratePerHour * 8;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ * 
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ * 
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {}
