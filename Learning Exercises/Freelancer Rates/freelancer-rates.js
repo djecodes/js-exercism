c6ab1ff57b130b7ba0420db59b66e41f0b761a53
Danny <90986300+djecodes@users.noreply.github.com>
2022-12-02 15:23:04 -0600

    Update freelancer-rates.js

--- Learning Exercises/Freelancer Rates/freelancer-rates.js
+++ Learning Exercises/Freelancer Rates/freelancer-rates.js
@@ -1,11 +1,21 @@
 /**
+ * The working hours in a day
+ */
+const WORKING_HOURS_IN_A_DAY = 8;
+
+/**
+ * The number of billable days in a month
+ */
+const BILLABLE_DAYS_IN_A_MONTH = 22;
+
+/**
  * The day rate, given a rate per hour
  * 
  * @param {number} ratePerHour
  * @return {number} the rate per day
  */
 export function dayRate(ratePerHour) {
-    return ratePerHour * 8;
+    return ratePerHour * WORKING_HOURS_IN_A_DAY;
 }
 
 /**
@@ -15,7 +25,9 @@ export function dayRate(ratePerHour) {
  * @param {number} ratePerHour: the rate per hour
  * @returns {number} the number of days
  */
-export function daysInBudget(budget, ratePerHour) {}
+export function daysInBudget(budget, ratePerHour) {
+    return Math.floor(budget / dayRate(ratePerHour));
+}
 
 /**
  * Calculates the discounted rate for large projects, rounded up
@@ -25,4 +37,11 @@ export function daysInBudget(budget, ratePerHour) {}
  * @param {number} discount: for example 20% written as 0.2
  * @returns {number} the rounded up discounted rate
  */
-export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {}
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+    const fullMonths = Math.floor(numDays / BILLABLE_DAYS_IN_A_MONTH);
+    const remainingDays = numDays % BILLABLE_DAYS_IN_A_MONTH;
+    const fullMonthDiscountPrice = fullMonths * BILLABLE_DAYS_IN_A_MONTH * dayRate(ratePerHour) * (1 - discount);
+    const remainingDaysPrice = remainingDays * dayRate(ratePerHour);
+
+    return Math.ceil(fullMonthDiscountPrice + remainingDaysPrice);
+}
